fe45a8e4613e68023488f29116d1593d961110f7
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-02 01:47:16 +1200

    Remove eslint-disable

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { produce } from "./produce";
 import { PrismaClient } from "@prisma/client";
 
