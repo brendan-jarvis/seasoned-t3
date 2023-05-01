de031dd23f5c1147de533153e4ecaa2059e75033
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-01 18:32:48 +1200

    Add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,13 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#B56b02",
+        secondary: "#3f6c51",
+        info: "#EFF6EE",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
