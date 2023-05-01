b6280ab55f444f505b40d8f3b4ab775589c9654a
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-01 17:42:52 +1200

    Init produce router

---
+++ src/server/api/routers/produce.ts
@@ -0,0 +1,122 @@
+import clerkClient from "@clerk/clerk-sdk-node";
+import { z } from "zod";
+import type { User } from "@clerk/nextjs/dist/api";
+import type { Produce } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+const availabilityType = z.enum([
+  "Available",
+  "Limited",
+  "Unavailable",
+  "Imported",
+  "LimitedImported",
+]);
+
+export const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profileImageUrl: user.profileImageUrl,
+  };
+};
+
+const addUserDataToAllProduce = async (allProduce: Produce[]) => {
+  const userId = allProduce.map((produce) => produce.authorId);
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: userId,
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return allProduce.map((produce) => {
+    const author = users.find((user) => user.id === produce.authorId);
+
+    if (!author) {
+      console.error("AUTHOR NOT FOUND", produce);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: `Author for produce not found. PRODUCE ID: ${produce.id}, USER ID: ${produce.authorId}`,
+      });
+    }
+
+    if (!author.username) {
+      author.username = "Seasoned User";
+    }
+
+    return {
+      produce,
+      author: {
+        ...author,
+        username: author.username ?? "Seasoned User",
+      },
+    };
+  });
+};
+
+export const produceRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const allProduce = await ctx.prisma.produce.findMany({
+      take: 10,
+      orderBy: { createdAt: "desc" },
+    });
+
+    return addUserDataToAllProduce(allProduce);
+  }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        type: z.enum([
+          "Vegetable",
+          "SpecialtyVegetable",
+          "Fruit",
+          "Berry",
+          "StoneFruit",
+          "SpecialtyFruit",
+          "EdibleFlower",
+          "Herb",
+        ]),
+        description: z.string().optional(),
+        image: z.string().optional(),
+        seasonality: z.object({
+          january: availabilityType,
+          february: availabilityType,
+          march: availabilityType,
+          april: availabilityType,
+          may: availabilityType,
+          june: availabilityType,
+          july: availabilityType,
+          august: availabilityType,
+          september: availabilityType,
+          october: availabilityType,
+          november: availabilityType,
+          december: availabilityType,
+        }),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const produce = await ctx.prisma.produce.create({
+        data: {
+          authorId,
+          title: input.title,
+          type: input.type,
+          description: input.description,
+          image: input.image,
+          seasonality: {
+            create: input.seasonality,
+          },
+        },
+      });
+
+      return produce;
+    }),
+});
