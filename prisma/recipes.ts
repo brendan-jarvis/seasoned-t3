62d5d66a7fb530f12e4e3bc915e74fb1904f3b95
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-02 22:15:40 +1200

    Finish recipe seed file

--- prisma/recipes.ts
+++ prisma/recipes.ts
@@ -10,151 +10,150 @@ export const recipes = [
     largeImage:
       "https://dish.co.nz/assets/Uploads/croquettes-hero-v2__FillWzg1MCwxMTc0XQ.jpg",
     ingredientSegments: {
-      create: {
-        "1": {
+      create: [
+        {
           title: "Ingredients",
           ingredients: {
-            create: {
-              "1": {
+            create: [
+              {
                 amount: "1.3",
                 unit: "kilogram",
                 name: "whole free-range chicken",
               },
-              "2": {
+              {
                 amount: "1",
                 unit: "",
                 name: "carrot, chopped",
               },
-              "3": {
+              {
                 amount: "1",
                 unit: "",
                 name: "onion, chopped",
               },
-              "4": {
+              {
                 amount: "8",
                 unit: "",
                 name: "whole cloves",
               },
-              "5": {
+              {
                 amount: "1/2",
                 unit: "tsp",
                 name: "allspice",
               },
-              "6": {
+              {
                 amount: "3",
                 unit: "",
                 name: "bay leaves",
               },
-            },
+            ],
           },
         },
-        "2": {
+        {
           title: "Filling",
           ingredients: {
-            create: {
-              "1": {
+            create: [
+              {
                 amount: "40",
                 unit: "grams",
                 name: "butter",
               },
-              "2": {
+              {
                 amount: "1",
                 unit: "",
                 name: "carrot, finely chopped",
               },
-              "3": {
+              {
                 amount: "1",
                 unit: "",
                 name: "onion, finely chopped",
               },
-              "4": {
+              {
                 amount: "",
                 unit: "",
                 name: "sea salt and freshly ground black pepper",
               },
-              "5": {
+              {
                 amount: "1/4",
                 unit: "cup",
                 name: "plain flour",
               },
-              "6": {
+              {
                 amount: "1 1/4",
                 unit: "cups",
                 name: "chilled stock",
               },
-              "7": {
+              {
                 amount: "1",
                 unit: "packet",
                 name: "chicken noodle soup",
               },
-            },
+            ],
           },
         },
-        "3": {
+        {
           title: "To assemble",
           ingredients: {
-            create: {
-              "1": {
+            create: [
+              {
                 amount: "5",
                 unit: "cups",
                 name: "breadcrumbs",
               },
-              "2": {
+              {
                 amount: "3",
                 unit: "",
                 name: "eggs",
               },
-              "3": {
+              {
                 amount: "5",
                 unit: "cups",
                 name: "rice bran or other neutral oil",
               },
-            },
+            ],
           },
         },
-        "4": {
+        {
           title: "To serve",
           ingredients: {
-            create: {
-              "1": {
+            create: [
+              {
                 amount: "",
                 unit: "",
                 name: "dijon mustard",
               },
-              "2": {
+              {
                 amount: "",
                 unit: "",
                 name: "chopped parsley",
               },
-            },
+            ],
           },
         },
-      },
+      ],
     },
     instructions: {
-      create: {
-        "1": {
+      create: [
+        {
           title: "Method",
           content:
             "Put the chicken in a large pot with all remaining ingredients and cover with water. Bring to a boil, reduce heat to a perky simmer and cook for 1¼ hours or until the chicken is just about falling off the bones. Remove the chicken from the pot, while leaving the stock to simmer on a low heat. Remove the meat from the chicken, putting the skin and carcass back into the stock. Chop the chicken into 1-2cm pieces and store, covered in the fridge. Simmer the stock, uncovered for a further 30 minutes, sieve to remove all the extra bits which you can now discard, and chill in the fridge for at least 4 hours or up to overnight.",
         },
-        "2": {
+        {
           title: "",
           content:
             "Skim the chilled fat from the stock. Melt the butter in a large heavy-bottomed pot and add the carrot and onion. Season well with salt and pepper and cook over a medium low heat for 10 minutes until both are softened. Add the flour and stir with a wooden spoon. Cook for 1 minute then add 1 cup of stock and whisk to form a smoothish paste. Whisk in a remaining stock and add the soup mix. Add the reserved chicken and stir in. Cook over a medium heat for 5 minutes or until the chicken soup noodles are softened and the sauce is thickened. Transfer to a container and chill for at least 4 hours.",
         },
-        "3": {
+        {
           title: "",
           content:
             "Line a baking tray with baking paper and put the breadcrumbs in a large bowl. Using about 1/3 cup at a time, take scoops of chilled chicken mixture (which should now be firm) and form them into even sausage shapes. Coat completely in breadcrumbs and place on the baking tray. Repeat until you have used all the chicken mix – you should have 12-13 croquettes. Whisk the eggs in a flat-bottomed bowl and one by one, dip the croquettes in the egg and roll again in breadcrumbs. Once they are all double-coated, chill for at least an hour, and up to overnight.",
         },
-        "4": {
+        {
           title: "",
           content:
             "Heat the oil in a saucepan until a cube of bread dropped in becomes golden and rises to the surface in about 20 seconds. Cook the croquettes in batches of about 4 at a time, for about 2-4 minutes, until golden and heated through. Drain well on paper towels and serve with a sprinkling of sea salt and parsley and Dijon mustard.",
         },
-      },
+      ],
     },
-    tags: {},
   },
 ];
