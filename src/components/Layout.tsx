2d15a424efee1ba712ee6db10ffc22d2bec19382
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-09 22:04:03 +1200

    Add Nav and Footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import type { PropsWithChildren } from "react";
+import { Nav } from "./Nav";
+import { Footer } from "./Footer";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
-    <main className="overflow-none flex h-screen justify-center">
-      <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
-        {props.children}
-      </div>
-    </main>
+    <>
+      <Nav />
+      <main className="overflow-none flex h-screen justify-center">
+        <div className="flex h-full w-full flex-col">{props.children}</div>
+      </main>
+      <Footer />
+    </>
   );
 };
