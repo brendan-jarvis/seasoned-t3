65d984b5ec9928848e379ac7dbaa1987513087f5
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-17 18:30:49 +1200

    Toaster

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import type { PropsWithChildren } from "react";
 import { Nav } from "./Nav";
 import { Footer } from "./Footer";
+import { Toaster } from "react-hot-toast";
 
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <>
       <Nav />
+      <Toaster />
       <main className="flex min-h-screen flex-col items-center justify-center bg-info">
         <div>{props.children}</div>
       </main>
