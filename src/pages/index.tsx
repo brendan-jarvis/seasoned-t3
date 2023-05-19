e8ca29477ad96cef9dc42c764cd8305501da03ce
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-19 18:30:05 +1200

    Display card list of in season produce

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,39 @@
 import { type NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 
 import { PageLayout } from "~/components/Layout";
+import { api } from "~/utils/api";
+import { AvailabilityType } from "@prisma/client";
 
 const Home: NextPage = () => {
+  const currentMonth = new Date()
+    .toLocaleString("default", { month: "long" })
+    .toLowerCase() as
+    | "january"
+    | "february"
+    | "march"
+    | "april"
+    | "may"
+    | "june"
+    | "july"
+    | "august"
+    | "september"
+    | "october"
+    | "november"
+    | "december";
+
+  const { data, isLoading } = api.produce.getAllByMonth.useQuery({
+    month: currentMonth,
+    availability: [AvailabilityType.Available],
+  });
+
+  const sortedProduce = data?.sort((a, b) => a.title.localeCompare(b.title));
+
+  const uniqueProduce = [
+    ...new Set(sortedProduce?.map((item) => item.title.split(" - ")[0])),
+  ];
+
   return (
     <>
       <Head>
@@ -25,6 +55,41 @@ const Home: NextPage = () => {
             <h1 className="text-5xl font-extrabold tracking-tight text-primary drop-shadow-md sm:text-[5rem]">
               Seasoned
             </h1>
+            <p className="font-sans text-sm text-gray-900">
+              Search ingredients in season to find delicious recipes!
+            </p>
+            <div>
+              <h2 className="text-xl font-bold tracking-tight text-secondary">
+                Seasonal Produce this Month
+              </h2>
+              <div className="grid max-w-2xl grid-flow-col gap-2 overflow-x-scroll p-2">
+                {isLoading ? (
+                  <p>Loading...</p>
+                ) : (
+                  uniqueProduce?.map((title, index) => (
+                    <div
+                      key={index}
+                      className="w-32 rounded-lg text-gray-950 shadow-xl drop-shadow-lg"
+                    >
+                      <figure>
+                        <Image
+                          src="/images/inigo-de-la-maza-s285sDw5Ikc-unsplash.jpg"
+                          width={500}
+                          height={500}
+                          alt={title ? title : "Produce"}
+                          className="rounded-t-lg"
+                        />
+                      </figure>
+                      <div className="">
+                        <h2 className="mt-6 pb-0 text-center font-sans font-medium">
+                          {title}
+                        </h2>
+                      </div>
+                    </div>
+                  ))
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </PageLayout>
