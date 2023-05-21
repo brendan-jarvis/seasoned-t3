5afd304784d61ac356a7a8bf96059bac0c7590e4
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-21 23:27:47 +1200

    Remove NavigationMenu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,32 +5,6 @@ import { Button } from "./ui/button";
 import { Toaster } from "react-hot-toast";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "~/components/ui/navigation-menu";
-
-const SeasonedLogo = () => (
-  <Link href="/">
-    <Button variant="ghost">
-      Seasoned
-      <Image
-        src="/images/fa-carrot.svg"
-        width={28}
-        height={28}
-        alt="Orange carrot icon"
-        className="bounce-once ml-1"
-      />
-    </Button>
-  </Link>
-);
-
 const Nav = () => {
   const links = [
     { href: "/recipes", label: "Recipes" },
@@ -38,47 +12,62 @@ const Nav = () => {
   ];
 
   return (
-    <NavigationMenu className="gap-2 bg-[#BF572B] py-8 font-serif font-extrabold drop-shadow-lg">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuLink>
-            <NavigationMenuLink href="/">
-              <Link
-                href="/"
-                className="font-serif hover:cursor-pointer hover:underline"
+    <div className="flex flex-row justify-center gap-2 bg-[#BF572B] py-8 font-serif font-extrabold drop-shadow-lg">
+      <Link
+        href="/"
+        className="font-serif hover:cursor-pointer hover:underline"
+      >
+        <Button variant="ghost" className="font-serif text-lg font-medium">
+          Seasoned
+          <Image
+            src="/images/fa-carrot.svg"
+            width={28}
+            height={28}
+            alt="Orange carrot icon"
+            className="bounce-once ml-1"
+          />
+        </Button>
+      </Link>
+      {links.map(({ href, label }, index) => (
+        <Button key={index} variant="ghost">
+          <Link
+            href={href}
+            className="font-serif hover:cursor-pointer hover:underline"
+          >
+            {label.toUpperCase()}
+          </Link>
+        </Button>
+      ))}
+      <SignedIn>
+        <Button variant="ghost">
+          <div className="indicator">
+            <Link href="/favourites">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
               >
-                <Button variant="ghost">
-                  Seasoned
-                  <Image
-                    src="/images/fa-carrot.svg"
-                    width={28}
-                    height={28}
-                    alt="Orange carrot icon"
-                    className="bounce-once"
-                  />
-                </Button>
-              </Link>
-            </NavigationMenuLink>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-        {links.map(({ href, label }, index) => (
-          <NavigationMenuItem key={index}>
-            <NavigationMenuLink href={href}>
-              <Button variant="ghost">
-                <Link
-                  href={href}
-                  className="font-serif hover:cursor-pointer hover:underline"
-                >
-                  {label.toUpperCase()}
-                </Link>
-              </Button>
-            </NavigationMenuLink>
-          </NavigationMenuItem>
-        ))}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                />
+              </svg>
+            </Link>
+          </div>
+        </Button>
+      </SignedIn>
+      <Button variant="ghost">
         <SignedIn>
-          <Button variant="ghost">
-            <div className="indicator">
-              <Link href="/favourites">
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton>
+            <Button variant="ghost" title="Sign in">
+              <div className="indicator">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   className="h-5 w-5"
@@ -90,42 +79,15 @@ const Nav = () => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth="2"
-                    d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
+                    d="M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z"
                   />
                 </svg>
-              </Link>
-            </div>
-          </Button>
-        </SignedIn>
-        <Button variant="ghost">
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-          <SignedOut>
-            <SignInButton>
-              <Button variant="ghost" title="Sign in">
-                <div className="indicator">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z"
-                    />
-                  </svg>
-                </div>
-              </Button>
-            </SignInButton>
-          </SignedOut>
-        </Button>
-      </NavigationMenuList>
-    </NavigationMenu>
+              </div>
+            </Button>
+          </SignInButton>
+        </SignedOut>
+      </Button>
+    </div>
   );
 };
 
