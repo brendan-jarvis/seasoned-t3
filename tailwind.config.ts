9195ac317222b5c424f77c7ad707be22728d0e64
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-21 23:27:40 +1200

    Custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -71,6 +71,10 @@ export default {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      fontFamily: {
+        sans: ["var(--font-nobile)"],
+        serif: ["var(--font-bree-serif)"],
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
