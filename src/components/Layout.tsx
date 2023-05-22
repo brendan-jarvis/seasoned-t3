1f6a7e1ff828fb2403a02b17ad7480d8215f4903
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-22 22:21:33 +1200

    Lucide heart

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "react-hot-toast";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
+import { Heart } from "lucide-react";
 
 const Nav = () => {
   const { theme, setTheme } = useTheme();
@@ -46,20 +47,7 @@ const Nav = () => {
         <Button variant="ghost">
           <div className="indicator">
             <Link href="/favourites">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="m12 21.35-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"
-                />
-              </svg>
+              <Heart />
             </Link>
           </div>
         </Button>
