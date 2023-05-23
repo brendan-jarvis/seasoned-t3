8eb0ca54248b9610a11c5b213510890f28f66491
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-05-23 22:41:16 +1200

    Add sidebar

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import type { PropsWithChildren } from "react";
 import { Button } from "./ui/button";
 import { Toaster } from "react-hot-toast";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
-import { Moon, Sun } from "lucide-react";
-import { Heart } from "lucide-react";
+import { Moon, Sun, Heart, Menu, X } from "lucide-react";
 
-const Nav = () => {
+const Nav = (props: PropsWithChildren) => {
   const { theme, setTheme } = useTheme();
+  const [sidebarVisibility, setsidebarVisibility] = useState(false);
 
   const links = [
     { href: "/recipes", label: "Recipes" },
@@ -17,84 +18,125 @@ const Nav = () => {
   ];
 
   return (
-    <div className="hidden flex-row justify-center bg-[#BF572B] py-8 text-center font-serif font-extrabold drop-shadow-lg lg:flex">
-      <Link
-        href="/"
-        className="font-serif hover:cursor-pointer hover:underline"
-      >
-        <Button variant="ghost" className="font-serif text-lg font-medium">
-          Seasoned
-          <Image
-            src="/images/fa-carrot.svg"
-            width={28}
-            height={28}
-            alt="Orange carrot icon"
-            className="bounce-once ml-1"
-          />
-        </Button>
-      </Link>
-      {links.map(({ href, label }, index) => (
-        <Button key={index} variant="ghost">
-          <Link
-            href={href}
-            className="font-serif hover:cursor-pointer hover:underline"
-          >
-            {label.toUpperCase()}
-          </Link>
-        </Button>
-      ))}
-      <SignedIn>
-        <Button variant="ghost">
-          <div className="indicator">
-            <Link href="/favourites">
-              <Heart />
-            </Link>
-          </div>
-        </Button>
-      </SignedIn>
-      <Button variant="ghost">
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-        <SignedOut>
-          <SignInButton>
-            <Button variant="ghost" title="Sign in">
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z"
-                  />
-                </svg>
-              </div>
+    <div>
+      <div className="flex flex-row justify-center bg-[#BF572B] py-8 text-center font-serif font-extrabold drop-shadow-lg">
+        <div className="lg:hidden">
+          <Button variant="ghost">
+            <Menu onClick={() => setsidebarVisibility(!sidebarVisibility)} />
+          </Button>
+        </div>
+        <Link
+          href="/"
+          className="font-serif hover:cursor-pointer hover:underline"
+        >
+          <Button variant="ghost" className="font-serif text-lg font-medium">
+            Seasoned
+            <Image
+              src="/images/fa-carrot.svg"
+              width={28}
+              height={28}
+              alt="Orange carrot icon"
+              className="bounce-once ml-1"
+            />
+          </Button>
+        </Link>
+        <div className="hidden lg:block">
+          {links.map(({ href, label }, index) => (
+            <Button key={index} variant="ghost">
+              <Link
+                href={href}
+                className="font-serif hover:cursor-pointer hover:underline"
+              >
+                {label.toUpperCase()}
+              </Link>
             </Button>
-          </SignInButton>
-        </SignedOut>
-      </Button>
-      {theme === "dark" ? (
-        <Button
-          variant="ghost"
-          title="Light mode"
-          onClick={() => setTheme("light")}
-        >
-          <Sun />
+          ))}
+        </div>
+        <SignedIn>
+          <Button variant="ghost">
+            <div className="indicator">
+              <Link href="/favourites">
+                <Heart />
+              </Link>
+            </div>
+          </Button>
+        </SignedIn>
+        <Button variant="ghost">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton>
+              <Button variant="ghost" title="Sign in">
+                <div className="indicator">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M11 7 9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z"
+                    />
+                  </svg>
+                </div>
+              </Button>
+            </SignInButton>
+          </SignedOut>
         </Button>
-      ) : (
-        <Button
-          variant="ghost"
-          title="Dark mode"
-          onClick={() => setTheme("dark")}
+        {theme === "dark" ? (
+          <Button
+            variant="ghost"
+            title="Light mode"
+            onClick={() => setTheme("light")}
+          >
+            <Sun />
+          </Button>
+        ) : (
+          <Button
+            variant="ghost"
+            title="Dark mode"
+            onClick={() => setTheme("dark")}
+          >
+            <Moon />
+          </Button>
+        )}
+      </div>
+      {props.children}
+
+      {sidebarVisibility && (
+        <div
+          className="fixed left-0 top-0 z-10 h-full w-full bg-black bg-opacity-50"
+          onClick={() => setsidebarVisibility(false)}
         >
-          <Moon />
-        </Button>
+          <div
+            className={`fixed left-0 top-0 z-20 h-full w-64 bg-background p-4 transition-transform duration-300 ${
+              sidebarVisibility ? "translate-x-0" : "translate-x-full"
+            }`}
+          >
+            <div className="mr-2 flex justify-end">
+              <X onClick={() => setsidebarVisibility(false)} />
+            </div>
+            {links.map(({ href, label }, index) => (
+              <Button
+                key={index}
+                variant="ghost"
+                className="mb-2 block w-full text-left"
+              >
+                <Link
+                  href={href}
+                  className="font-serif hover:cursor-pointer hover:underline"
+                >
+                  {label.toUpperCase()}
+                </Link>
+              </Button>
+            ))}
+          </div>
+        </div>
       )}
     </div>
   );
@@ -140,9 +182,8 @@ export const PageLayout = (props: PropsWithChildren) => {
   return (
     <>
       <Toaster />
-      <Nav />
       <main className="min-h-screen items-center justify-center bg-background text-foreground dark:bg-background dark:text-foreground">
-        {props.children}
+        <Nav>{props.children}</Nav>
       </main>
       <Footer />
     </>
