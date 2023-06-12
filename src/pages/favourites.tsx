8e43461aa7b1e4ea2a3311fec37b253871cf8827
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-06-12 21:07:53 +1200

    Display favourites, can delete favourite

--- src/pages/favourites.tsx
+++ src/pages/favourites.tsx
@@ -1,8 +1,22 @@
 import { type NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
 
 import { PageLayout } from "~/components/Layout";
 import { LoadingSpinner } from "~/components/LoadingSpinner";
+import dayjs from "dayjs";
+import { toast } from "react-hot-toast";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import { Button } from "~/components/ui/button";
+import { CheckCircle, MinusCircle, Trash2, Star } from "lucide-react";
 
 import { api } from "~/utils/api";
 
@@ -13,6 +27,27 @@ const Favourites: NextPage = () => {
     error,
   } = api.favourites.getAll.useQuery();
 
+  const { mutate, isLoading: isDeletingFavourite } =
+    api.favourites.deleteOne.useMutation({
+      onSuccess: () => {
+        toast.success(`Removed recipe from favorites!`);
+      },
+      onError: (e) => {
+        const errorMessage = e.data?.zodError?.fieldErrors.content;
+        const errorCode = e.data?.code;
+        if (errorMessage && errorMessage[0]) {
+          toast.error(errorMessage[0]);
+        }
+        if (errorCode === "INTERNAL_SERVER_ERROR") {
+          toast.error("Recipe already deleted!");
+        } else {
+          toast.error(
+            "Failed to remove recipe from favorites! Please try again later."
+          );
+        }
+      },
+    });
+
   if (isLoading) {
     return (
       <PageLayout>
@@ -20,8 +55,6 @@ const Favourites: NextPage = () => {
       </PageLayout>
     );
   }
-
-  console.log(favourites);
 
   return (
     <>
@@ -44,7 +77,7 @@ const Favourites: NextPage = () => {
             Favourites
           </h1>
 
-          {error && (
+          {error ? (
             <>
               <h2 className="text-center font-serif text-xl text-destructive">
                 {error.message === "UNAUTHORIZED"
@@ -55,6 +88,58 @@ const Favourites: NextPage = () => {
                 Please sign in to view your favourites.
               </p>
             </>
+          ) : (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Title</TableHead>
+                  <TableHead>Rating</TableHead>
+                  <TableHead>Completed</TableHead>
+                  <TableHead>Added On</TableHead>
+                  <TableHead>Last Updated</TableHead>
+                  <TableHead>Delete</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {favourites.map((favourite) => (
+                  <TableRow key={favourite.recipeId}>
+                    <TableCell>
+                      <Link
+                        href={`/recipes/${favourite.recipeId}`}
+                        className="text-blue-500 hover:text-blue-700 focus:text-blue-700"
+                      >
+                        {favourite.recipe.title}
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      {favourite.rating ? favourite.rating : "No rating yet."}
+                    </TableCell>
+                    <TableCell>
+                      {favourite.completed ? <CheckCircle /> : <MinusCircle />}
+                    </TableCell>
+                    <TableCell>
+                      {dayjs(favourite.createdAt).format("D MMM YYYY")}
+                    </TableCell>
+                    <TableCell>
+                      {dayjs(favourite.updatedAt).format("D MMM YYYY")}
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        variant="outline"
+                        onClick={() => {
+                          mutate({
+                            recipeId: favourite.recipeId,
+                          });
+                        }}
+                        className="px-2"
+                      >
+                        <Trash2 />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
           )}
         </div>
       </PageLayout>
