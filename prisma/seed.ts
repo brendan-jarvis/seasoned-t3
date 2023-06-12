077ba5c3319187ed76c98707e664ff8c6d78c808
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-06-12 16:33:37 +1200

    Working on price seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,52 @@
-import { produce } from "./produce";
 import { PrismaClient } from "@prisma/client";
+import { produce } from "./produce";
 import { recipes } from "./recipes";
+import { prices } from "./prices";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  // Add produce
   // for (const item of produce) {
   //   await prisma.produce.create({
   //     data: item,
   //   });
   // }
-  for (const recipe of recipes) {
-    // await prisma.recipe.create({
-    //   data: recipe,
-    // });
-    const createdAt = new Date(recipe.createdAt);
-    await prisma.recipe.create({
-      data: {
-        ...recipe,
-        createdAt,
-      },
-    });
-  }
+  //
+  //
+  //
+  // Add recipes with createdAt
+  // for (const recipe of recipes) {
+  // await prisma.recipe.create({
+  //   data: recipe,
+  // });
+  // const createdAt = new Date(recipe.createdAt);
+  // await prisma.recipe.create({
+  //   data: {
+  //     ...recipe,
+  //     createdAt,
+  //   },
+  // });
+  // }
+  // Add prices
+  // for (const price of prices) {
+  //   await prisma.price.create({
+  //     data: {
+  //       name: price.name,
+  //       year_month: price.year_month,
+  //       price: price.price,
+  //       produce: {
+  //         connect: {
+  //           where: {
+  //             name: {
+  //               contains: price.name,
+  //             },
+  //           },
+  //         },
+  //       },
+  //     },
+  //   });
+  // }
 }
 
 main()
