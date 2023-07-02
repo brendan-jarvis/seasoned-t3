15c84a269e0b800f48a262620f2061ca12c5b362
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-07-03 00:13:06 +1200

    getCurrentMonth

--- src/utils/func.ts
+++ src/utils/func.ts
@@ -4,3 +4,18 @@ export const capitaliseFirstLetters = (string: string) => {
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(" ");
 };
+
+export const getCurrentMonth = () =>
+  new Date().toLocaleString("default", { month: "long" }).toLowerCase() as
+    | "january"
+    | "february"
+    | "march"
+    | "april"
+    | "may"
+    | "june"
+    | "july"
+    | "august"
+    | "september"
+    | "october"
+    | "november"
+    | "december";
