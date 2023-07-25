95f7f256ba6be6596398f95df4d23d38f6a8ca18
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-07-25 14:27:51 +1200

    Max width two cards wide and center recipe cards

--- src/pages/recipes.tsx
+++ src/pages/recipes.tsx
@@ -120,7 +120,7 @@ const Recipes: NextPage = () => {
             </p>
           ) : (
             <>
-              <div className="mb-4 flex flex-wrap justify-center gap-4">
+              <div className="mx-auto mb-4 flex max-w-[800px] flex-wrap justify-center gap-4">
                 {allRecipes.map((recipe: Recipe) => (
                   <Card key={recipe.id} className="w-[350px]">
                     <Link href={`/recipes/${recipe.id}`}>
