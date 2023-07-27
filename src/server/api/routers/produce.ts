31f81c8ab43cede5fb1c02d0ff1b7d6c2283fda2
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-07-27 22:14:57 +1200

    Add redis getAllByMonth

--- src/server/api/routers/produce.ts
+++ src/server/api/routers/produce.ts
@@ -59,14 +59,6 @@ export const produceRouter = createTRPCRouter({
       })
     )
     .query(async ({ ctx, input }) => {
-      const cachedAllProduceByMonth = await redis.get(
-        `produce/getAllByMonth?month=${input.month}&seasonality=${input.seasonality}`
-      );
-
-      if (cachedAllProduceByMonth) {
-        return cachedAllProduceByMonth;
-      }
-
       const allProduce = await ctx.prisma.produce.findMany({
         where: {
           seasonality: {
@@ -76,14 +68,6 @@ export const produceRouter = createTRPCRouter({
         orderBy: { type: "asc" },
         include: { seasonality: true, availability: true },
       });
-
-      await redis.set(
-        `produce/getAllByMonth?month=${input.month}&seasonality=${input.seasonality}`,
-        JSON.stringify(allProduce),
-        {
-          ex: 43200, // 12 hours
-        }
-      );
 
       return allProduce;
     }),
